ed0c5e3e805d2c5cd1e56d94d3d2bc4251b7f930
yassineChihab <79338067+yassineChihab@users.noreply.github.com>
2023-09-06 22:21:49 +0100

    section 2

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,10 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
 var app = builder.Build();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
